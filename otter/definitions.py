0c435033d3ebbe24c41bff6df3e0dfc8a19fb58f
Adam <adam.s.tuft@gmail.com>
2021-09-14 11:52:34 +0100

    Add src/python/otter/definitions.py to allow factoring out string literals into Enum subclass

---
+++ otter/definitions.py
@@ -0,0 +1,65 @@
+from enum import Enum
+
+class EventType(str, Enum):
+    thread_begin = "thread_begin"
+    thread_end = "thread_end"
+    parallel_begin = "parallel_begin"
+    parallel_end = "parallel_end"
+    workshare_begin = "workshare_begin"
+    workshare_end = "workshare_end"
+    sync_begin = "sync_begin"
+    sync_end = "sync_end"
+    task_create = "task_create"
+    task_schedule = "task_schedule"
+    task_enter = "task_enter"
+    task_leave = "task_leave"
+    master_begin = "master_begin"
+    master_end = "master_end"
+
+class RegionType(str, Enum):
+    parallel = "parallel"
+    workshare = "workshare"
+    sync = "sync"
+    task = "task"
+    initial_task = "initial_task"
+    implicit_task = "implicit_task"
+    explicit_task = "explicit_task"
+    target_task = "target_task"
+    sections = "sections"
+    single_executor = "single_executor"
+    single_other = "single_other"
+    distribute = "distribute"
+    loop = "loop"
+    taskloop = "taskloop"
+    master = "master"
+    barrier = "barrier"
+    barrier_implicit = "barrier_implicit"
+    barrier_explicit = "barrier_explicit"
+    barrier_implementation = "barrier_implementation"
+    taskwait = "taskwait"
+    taskgroup = "taskgroup"
+
+class TaskStatus(str, Enum):
+    complete = "complete"
+    taskyield = "yield"
+    cancel = "cancel"
+    detach = "detach"
+    early_fulfil = "early_fulfil"
+    late_fulfil = "late_fulfil"
+    switch = "switch"
+
+class TaskType(str, Enum):
+    initial = "initial_task"
+    implicit = "implicit_task"
+    explicit = "explicit_task"
+    target = "target_task"
+
+class Endpoint(str, Enum):
+    enter = "enter"
+    leave = "leave"
+    discrete = "discrete"
+
+class EdgeType(str, Enum):
+    execution_flow = "execution_flow"
+    taskwait = "taskwait"
+    taskgroup = "taskgroup"
