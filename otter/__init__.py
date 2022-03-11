d057f6e09ac0cdb95ca64d1ae05be8d2fecd51ce
Adam <adam.s.tuft@gmail.com>
2022-03-11 14:59:04 +0000

    Add runtime check for graphvix

--- otter/__init__.py
+++ otter/__init__.py
@@ -7,3 +7,11 @@ Key functionality:
 """
 
 from .args import get_args
+
+from shutil import which
+
+# Check that the "dot" commandline utility is available
+if which("dot") is None:
+    print(f"Error: {__name__} couldn't find the graphviz command line utility \"dot\" (see https://graphviz.org/download/).")
+    print("Please install graphviz before continuing.")
+    quit()
