12e1b8524078f9f422d95cb2ee21fc0c592a46f1
Adam <adam.s.tuft@gmail.com>
2022-03-28 13:41:37 +0100

    Add logging statements

--- otter/TaskRegistry/TaskRegistry.py
+++ otter/TaskRegistry/TaskRegistry.py
@@ -3,6 +3,7 @@ from ..EventFactory import events
 from ..definitions import Attr
 from ..utils import PrettyCounter
 from .tasks import Task
+from ..Logging import get_logger
 
 class TaskRegistry:
     """
@@ -11,7 +12,9 @@ class TaskRegistry:
     """
 
     def __init__(self):
+        self.log = get_logger(f"{self.__class__.__name__}")
         self._dict = dict()
+        self.log.debug(f"initialised {self}")
 
     def __getitem__(self, uid: int) -> Union[Task, None]:
         if uid not in self._dict:
@@ -30,9 +33,10 @@ class TaskRegistry:
 
     def register_task(self, e: events._Event) -> Task:
         t = Task(e)
+        self.log.debug(f"registering task {t.id} (parent={t.parent_id if t.id>0 else None})")
         if t.id in self._dict:
             raise ValueError(f"task {t.id} was already registered in {self}")
         self._dict[t.id] = t
         if t.id > 0:
             self[t.parent_id].append_child(t.id)
-        return self._dict[e.unique_id]
+        return t
