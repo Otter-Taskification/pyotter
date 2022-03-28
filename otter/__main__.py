41a41907221469b8c3cfb473bf6528f7893acf17
Adam <adam.s.tuft@gmail.com>
2022-03-28 13:43:08 +0100

    Add logging

--- otter/__main__.py
+++ otter/__main__.py
@@ -2,18 +2,16 @@ import otf2
 import otter
 
 args = otter.get_args()
-event_counter = otter.utils.PrettyCounter()
+otter.Logging.initialise(args)
+log = otter.get_logger('MAIN')
 
-print(f"loading OTF2 anchor file: {args.anchorfile}")
+log.info(f"{args.anchorfile=}")
 with otf2.reader.open(args.anchorfile) as r:
     events = otter.EventFactory(r)
-    tr = otter.TaskRegistry()
-    for event in events:
-        event_counter[type(event)] += 1
-        if event.is_task_register_event:
-            tr.register_task(event)
+    tasks = otter.TaskRegistry()
+    chunks = otter.ChunkFactory(events, tasks)
+    log.debug(f"iterating over chunks in {chunks}")
+    for chunk in chunks:
+        pass
 
-print(event_counter)
-print(tr)
-for t in tr:
-    print(t.id, tuple(t.children))
+log.info("Done!")
