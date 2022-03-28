a05a5454c1c3af0572fc1968c1fa01a1d8428485
Adam <adam.s.tuft@gmail.com>
2022-03-28 13:36:22 +0100

    Add alias for otf2.definitions.Location

--- otter/types/aliases.py
+++ otter/types/aliases.py
@@ -5,6 +5,7 @@ import otf2
 # Alias some OTF2 types
 OTF2Event = Union[otf2.events._Event]
 OTF2Reader = Union[otf2.reader.Reader]
+OTF2Location = Union[otf2.definitions.Location]
 OTF2DefinitionsRegistry = Union[otf2.registry.DefinitionRegistry]
 OTF2Attribute = Union[Any]
 
