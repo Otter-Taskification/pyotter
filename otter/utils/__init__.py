68316c7ba749c4032651e83b85305f255f918537
Adam <adam.s.tuft@gmail.com>
2022-03-29 19:39:38 +0100

    Add new imports

--- otter/utils/__init__.py
+++ otter/utils/__init__.py
@@ -1,8 +1,9 @@
 from ..logging import get_logger
 from .counters import PrettyCounter, VertexLabeller
 from .vertex_predicates import key_is_not_none, is_region_type, is_empty_task_region
-from .vertex_attr_handlers import drop_args, unique_arg, unique_or_none
+from .vertex_attr_handlers import drop_args, unique_arg, unique_or_none, make_event_combiner
 from . import decorate
+from . import vertex_attr_handlers as handlers
 
 def default_attribute_handler(names, logger=None):
     if logger is None:
@@ -10,5 +11,5 @@ def default_attribute_handler(names, logger=None):
     h = dict()
     for name in names:
         h[name] = decorate.log_call_with_msg(drop_args, f"combining attribute: {name}", logger)
-        logger.info(f"({__package__}) set attribute handler 'otter.utils.drop_args' for vertex attribute '{name}'")
+        logger.info(f"({__package__}) set attribute handler '{drop_args.__module__}.{drop_args.__name__}' for vertex attribute '{name}'")
     return h
