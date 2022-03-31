322b610e304f040d86647a38a91e926cf070a4e7
Adam <adam.s.tuft@gmail.com>
2022-03-31 17:17:38 +0100

    Rename class

--- otter/utils/__init__.py
+++ otter/utils/__init__.py
@@ -1,4 +1,4 @@
 from .counters import VertexLabeller
 from .vertex_predicates import key_is_not_none, is_region_type, is_empty_task_region
-from .vertex_attr_handlers import EventCombiner, AttributeHandlerTable
+from .vertex_attr_handlers import VertexAttributeCombiner, AttributeHandlerTable
 from . import vertex_attr_handlers as handlers
