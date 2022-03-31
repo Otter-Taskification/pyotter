791da65610ed83941e57f76d2282a44f548bef2b
Adam <adam.s.tuft@gmail.com>
2022-03-31 12:03:59 +0100

    Abstract behaviour for passing single-exec/master events into internal function

--- otter/utils/vertex_attr_handlers.py
+++ otter/utils/vertex_attr_handlers.py
@@ -18,7 +18,7 @@ def drop_args(args):
     return None
 
 
-def pass_single_executor(args):
+def _pass_unique_event(args, region_type: RegionType):
     """
     Given a List[events._Event], return a single-exec event from the set of such events.
     If no such events present, return the original list.
@@ -29,32 +29,28 @@ def pass_single_executor(args):
     if len(args) == 1:
         raise RuntimeError(f"shouldn't pass list of 1 event: {args}")
     # args is a list of >1 events._Event
-    single_executor_events = set(e for e in args if e.region_type == RegionType.single_executor)
-    if len(single_executor_events) > 1:
-        # error: if >1 single-exec arrived here, the vertices were mis-labelled by single region somehow
-        module_logger.error(f"multiple single-executor events received: {args}")
-        raise ValueError(f"multiple single-executor events received: {args}")
-    elif len(single_executor_events) == 1:
-        event = single_executor_events.pop()
-        module_logger.debug(f"returning single event: {event}")
+    unique_events = set(e for e in args if e.region_type == region_type)
+    if len(unique_events) > 1:
+        # error: if >1 event arrived here, the vertices were somehow mis-labelled
+        module_logger.error(f"multiple {region_type} events received: {args}")
+        raise ValueError(f"multiple {region_type} events received: {args}")
+    elif len(unique_events) == 1:
+        event = unique_events.pop()
+        module_logger.debug(f"returning event: {event}")
         return event
     else:
         event_types = set(type(e).__name__ for e in args)
-        module_logger.debug(f"no single-executor events, returning event list ({event_types=})")
+        module_logger.debug(f"no {region_type} events, returning event list ({event_types=})")
         return args
 
 
+
+def pass_single_executor(args):
+    return _pass_unique_event(args, RegionType.single_executor)
+
+
 def pass_unique_master_event(args):
-    assert isinstance(args, list)
-    assert are(all, args, events._Event)
-    if len(args) == 1:
-        raise RuntimeError(f"shouldn't pass list of 1 event: {args}")
-    # args is a list of >1 events._Event
-    if any(e.region_type != RegionType.master for e in args):
-        raise TypeError(f"expected only master events, got {set(e.region_type for e in args)}")
-    else:
-        assert len({args}) # only ever expect 1 unique master event
-        return args[0]
+    return _pass_unique_event(args, RegionType.master)
 
 
 class AttributeHandlerTable(dict):
