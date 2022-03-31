73d302436b53cb75bfbe7b8598c450a7977e8832
Adam <adam.s.tuft@gmail.com>
2022-03-31 10:58:05 +0100

    Add pass_unique_master_event, fix pass_single_executor to assert that at most 1 single-exec event was passed

--- otter/utils/vertex_attr_handlers.py
+++ otter/utils/vertex_attr_handlers.py
@@ -1,4 +1,5 @@
 from functools import wraps
+from itertools import chain
 from logging import DEBUG, INFO
 from ..logging import get_logger
 from ..definitions import RegionType
@@ -17,15 +18,23 @@ def drop_args(args):
     return None
 
 
-# @log_args(module_logger)
 def pass_single_executor(args):
+    """
+    Given a List[events._Event], return a single-exec event from the set of such events.
+    If no such events present, return the original list.
+    When combining events, it is an error if >1 single-exec event is encountered for a given vertex
+    """
     assert isinstance(args, list)
     assert are(all, args, events._Event)
     if len(args) == 1:
         raise RuntimeError(f"shouldn't pass list of 1 event: {args}")
     # args is a list of >1 events._Event
     single_executor_events = set(e for e in args if e.region_type == RegionType.single_executor)
-    if any(single_executor_events):
+    if len(single_executor_events) > 1:
+        # error: if >1 single-exec arrived here, the vertices were mis-labelled by single region somehow
+        module_logger.error(f"multiple single-executor events received: {args}")
+        raise ValueError(f"multiple single-executor events received: {args}")
+    elif len(single_executor_events) == 1:
         event = single_executor_events.pop()
         module_logger.debug(f"returning single event: {event}")
         return event
@@ -33,6 +42,19 @@ def pass_single_executor(args):
         event_types = set(type(e).__name__ for e in args)
         module_logger.debug(f"no single-executor events, returning event list ({event_types=})")
         return args
+
+
+def pass_unique_master_event(args):
+    assert isinstance(args, list)
+    assert are(all, args, events._Event)
+    if len(args) == 1:
+        raise RuntimeError(f"shouldn't pass list of 1 event: {args}")
+    # args is a list of >1 events._Event
+    if any(e.region_type != RegionType.master for e in args):
+        raise TypeError(f"expected only master events, got {set(e.region_type for e in args)}")
+    else:
+        assert len({args}) # only ever expect 1 unique master event
+        return args[0]
 
 
 class AttributeHandlerTable(dict):
