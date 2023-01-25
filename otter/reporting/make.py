10c4ad34485e1a452042a0782943959ac323abc7
Adam <73994535+adamtuft@users.noreply.github.com>
2023-01-25 11:26:30 +0000

    Add make.graphviz_record_table for HTML-like table of a set of attributes

--- otter/reporting/make.py
+++ otter/reporting/make.py
@@ -25,3 +25,13 @@ def table(keys: list, tidy_keys: dict, rows: list, **kwargs):
                         trow.append(wrap("td", row.get(key, "NONE")))
 
     return t
+
+def graphviz_record_table(object_attr: dict, **kwargs):
+    """A HTML-like table where each row represents one key-value pair of some object's attributes"""
+    attr = defaultdict(lambda: dict(), kwargs.get("attr", dict()))
+    with tag("table", **attr["table"]) as table:
+        for key, value in object_attr.items():
+            with table.add(tag("tr", **attr["tr"])) as row:
+                row.append(wrap("td", wrap("b", key), **attr["td"]))
+                row.append(wrap("td", value, **attr["td"]))
+    return table
