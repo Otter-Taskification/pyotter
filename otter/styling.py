b9ecd73bf66a16432a482e1222044cca8baacda9
Adam <73994535+adamtuft@users.noreply.github.com>
2023-03-16 14:20:53 +0000

    Make styling maps regular dicts, add StyleVertexShapeAsRegionType to get shape and colour from region type of a vertex

--- otter/styling.py
+++ otter/styling.py
@@ -1,11 +1,13 @@
-from collections import defaultdict
+from typing import Tuple
+from igraph import Graph
 from . import log
 from . import definitions as defn
+from . import graph_styling
 
 get_module_logger = log.logger_getter("styling")
 
 # Map region type to node color
-colormap_region_type = defaultdict(lambda: 'grey', **{
+colormap_region_type = { # default grey
     'initial_task': 'green',
     'implicit_task': 'fuchsia',
     'explicit_task': 'cyan',
@@ -26,14 +28,14 @@ colormap_region_type = defaultdict(lambda: 'grey', **{
     # For colouring by endpoint
     'enter': 'green',
     'leave': 'red'
-})
+}
 
-colormap_edge_type = defaultdict(lambda: 'black', **{
+colormap_edge_type = { # default black
     'taskwait': 'red',
     'taskgroup': 'red',
-})
+}
 
-shapemap_region_type = defaultdict(lambda: 'circle', **{
+shapemap_region_type = { # default to circle
     'initial_task': 'square',
     'implicit_task': 'square',
     'explicit_task': 'square',
@@ -55,9 +57,9 @@ shapemap_region_type = defaultdict(lambda: 'circle', **{
 
     # Phases
     'generic_phase': 'box'
-})
+}
 
-task_attribute_names = defaultdict(lambda: 'UNDEFINED', **{
+task_attribute_names = { # default to "UNDEFINED"
     "id": "Unique ID",
     "parent_id": "Parent ID",
     "task_type": "Task Type",
@@ -71,14 +73,14 @@ task_attribute_names = defaultdict(lambda: 'UNDEFINED', **{
     "source_file_name": "Source File",
     "source_func_name": "Source Function",
     "source_line_number": "Source Line No."
-})
+}
 
-colormap_task_type = defaultdict(lambda: 'black', **{
+colormap_task_type = { # default to black
     'initial_task': 'cyan',
     'implicit_task': 'gray',
     'explicit_task': 'red',
     'target_task': 'orange'
-})
+}
 
 def style_tasks(t):
     logger = get_module_logger()
@@ -96,7 +98,19 @@ def style_tasks(t):
     t.vs['shape'] = 'square'
     t.vs['color'] = [colormap_task_type[t.task_type] for t in tasks]
 
-def style_graph(graph):
+
+class StyleVertexShapeAsRegionType(graph_styling.BaseGraphStyle):
+
+    def get_vertex_style(self, vertex) -> Tuple[graph_styling.VertexStyle, str]:
+        label = vertex['vertex_label'] or " "
+        style = graph_styling.VertexStyle(
+            "filled",
+            shapemap_region_type.get(vertex["vertex_color_key"], "circle"),
+            colormap_region_type.get(vertex["vertex_shape_key"], "fuchsia")
+        )
+        return style, label
+
+def style_graph(graph: Graph, style: graph_styling.GraphStylingProtocol = StyleVertexShapeAsRegionType()):
 
     assert(defn.Attr.edge_type in graph.es.attribute_names())
 
@@ -114,7 +128,14 @@ def style_graph(graph):
         logger.debug("defining vertex labels")
         graph.vs['label'] = [v['vertex_label'] or " " for v in graph.vs]
 
-    graph.vs['style'] = "filled"
-    graph.vs['shape'] = [shapemap_region_type[key] for key in graph.vs["vertex_color_key"]]
-    graph.vs['color'] = [colormap_region_type[key] for key in graph.vs["vertex_shape_key"]]
-    graph.es['color'] = [colormap_edge_type[key] for key in graph.es[defn.Attr.edge_type]]
+    per_vertex_styles_labels = [style.get_vertex_style(vertex) for vertex in graph.vs]
+    vertex_styles_tuples, vertex_labels = list(map(list, zip(*per_vertex_styles_labels)))
+    vertex_styles, vertex_shapes, vertex_colors = list(map(list, zip(*vertex_styles_tuples)))
+    graph.vs['style'] = vertex_styles
+    graph.vs['shape'] = vertex_shapes
+    graph.vs['color'] = vertex_colors
+
+    # graph.vs['style'] = "filled"
+    # graph.vs['shape'] = [shapemap_region_type[key] for key in graph.vs["vertex_color_key"]]
+    # graph.vs['color'] = [colormap_region_type[key] for key in graph.vs["vertex_shape_key"]]
+    # graph.es['color'] = [colormap_edge_type[key] for key in graph.es[defn.Attr.edge_type]]
