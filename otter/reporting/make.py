bb7a33973a448461747deaef85eefeec7728206b
Adam <73994535+adamtuft@users.noreply.github.com>
2023-06-13 13:42:44 +0100

    Better handling of missing values

--- otter/reporting/make.py
+++ otter/reporting/make.py
@@ -7,7 +7,7 @@ def wrap(name, content, **kwargs):
             t.append(content)
     return t
 
-def table(keys: list, tidy_keys: dict, rows: list, **kwargs):
+def table(keys: list, tidy_keys: dict, rows: list, na_value = None, **kwargs):
 
     attr = defaultdict(lambda: dict(), kwargs.get("attr", dict()))
 
@@ -16,13 +16,16 @@ def table(keys: list, tidy_keys: dict, rows: list, **kwargs):
         with t.add(tag("thead")) as thead:
             with thead.add(tag("tr", **attr["tr"])) as trow:
                 for key in keys:
-                    trow.append(wrap("th", tidy_keys.get(key, "NONE")))
+                    trow.append(wrap("th", tidy_keys.get(key, f"UNKNOWN COLUMN {key}")))
 
         with t.add(tag("tbody")) as tbody:
             for row in rows:
                 with tbody.add(tag("tr")) as trow:
                     for key in keys:
-                        trow.append(wrap("td", row.get(key, "NONE")))
+                        value = row.get(key, f"NO VALUE FOR KEY {key}")
+                        if value is None and na_value is not None:
+                            value = na_value
+                        trow.append(wrap("td", value))
 
     return t
 
