143534e6040d06039cc5d57544ab5d55be5d369e
Adam <73994535+adamtuft@users.noreply.github.com>
2023-06-13 13:38:17 +0100

    Update task_attributes view, add script to count child tasks per set of parent attributes

---
+++ otter/db/_scripts/tasks_count_children_by_attributes.sql
@@ -0,0 +1,30 @@
+select attr.task_label
+    ,attr.flavour
+    ,attr.init_file
+    ,attr.init_line
+    ,attr.init_func
+    ,attr.start_file
+    ,attr.start_line
+    ,attr.start_func
+    ,attr.end_file
+    ,attr.end_line
+    ,attr.end_func
+    ,child.task_label as child_task_label
+    ,child.flavour as child_flavour
+    ,child.init_file as child_init_file
+    ,child.init_line as child_init_line
+    ,child.init_func as child_init_func
+    ,child.start_file as child_start_file
+    ,child.start_line as child_start_line
+    ,child.start_func as child_start_func
+    ,child.end_file as child_end_file
+    ,child.end_line as child_end_line
+    ,child.end_func as child_end_func
+    ,count(distinct child.id) as child_tasks
+from task_attributes as attr
+left join task_attributes as child
+    on child.parent_id = attr.id
+group by 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22
+order by attr.task_label
+    ,child_tasks desc
+;
--- otter/db/_scripts/tasks_create_views.sql
+++ otter/db/_scripts/tasks_create_views.sql
@@ -2,12 +2,12 @@
 
 -- Get a readable view of the tasks' initialisation locations
 create view if not exists task_init_location(
-         task
+         id
         ,file
         ,func
         ,line
     ) as
-    select task.id as task
+    select task.id as id
         ,file.text as file
         ,func.text as func
         ,src.line as line
@@ -22,12 +22,12 @@ create view if not exists task_init_location(
 
 -- Get a readable view of the tasks' start locations
 create view if not exists task_start_location(
-         task
+         id
         ,file
         ,func
         ,line
     ) as
-    select task.id as task
+    select task.id as id
         ,file.text as file
         ,func.text as func
         ,src.line as line
@@ -42,12 +42,12 @@ create view if not exists task_start_location(
 
 -- Get a readable view of the tasks' start locations
 create view if not exists task_end_location(
-         task
+         id
         ,file
         ,func
         ,line
     ) as
-    select task.id as task
+    select task.id as id
         ,file.text as file
         ,func.text as func
         ,src.line as line
@@ -73,18 +73,37 @@ create view if not exists task_location as
     select *,
            'end' as type
     from task_end_location
-    order by task
+    order by id
+        ,type
 ;
 
 -- A readable view of a task's attributes (flavour, label, etc)
 create view if not exists task_attributes as
     select task.id
+        ,parent.parent_id
         ,task.flavour
         ,string.text as task_label
         ,task.start_ts
         ,task.end_ts
         ,task.duration
+        ,init_loc.file as init_file
+        ,init_loc.func as init_func
+        ,init_loc.line as init_line
+        ,start_loc.file as start_file
+        ,start_loc.func as start_func
+        ,start_loc.line as start_line
+        ,end_loc.file as end_file
+        ,end_loc.func as end_func
+        ,end_loc.line as end_line
     from task
+    left join task_relation as parent
+        on task.id = parent.child_id
     left join string
-    on task.user_label = string.id
+        on task.user_label = string.id
+    left join task_init_location as init_loc
+        on task.id = init_loc.id
+    left join task_start_location as start_loc
+        on task.id = start_loc.id
+    left join task_end_location as end_loc
+        on task.id = end_loc.id
 ;
