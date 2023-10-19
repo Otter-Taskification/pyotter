8a5afab45fa3da326f6124fa87765686d7a1aeae
Adam <73994535+adamtuft@users.noreply.github.com>
2023-10-19 13:04:56 +0100

    Remove file

--- otter/reader.py
+++
@@ -1,9 +0,0 @@
-from __future__ import annotations
-
-from contextlib import closing
-
-from otter.otf2_ext.reader import OTF2Reader
-
-def get_otf2_reader(*args, **kwargs) -> closing[OTF2Reader]:
-    # allows _OTF2Reader to be used in a with-block
-    return closing(OTF2Reader(*args, **kwargs))
