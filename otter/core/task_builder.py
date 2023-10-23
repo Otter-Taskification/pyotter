ff245d03764cae6459be6876602412d562b5fa4b
Adam <73994535+adamtuft@users.noreply.github.com>
2023-10-23 13:17:40 +0100

    DBTaskBuilder responsible for mapping source locations and labels to int keys

--- otter/core/task_builder.py
+++ otter/core/task_builder.py
@@ -1,33 +1,32 @@
 from __future__ import annotations
 
-from typing import List, Tuple
+from typing import List, Tuple, Dict, Optional
 import sqlite3
 
 from otter import db
-
+from otter.definitions import SourceLocation, TaskAction
 
 class DBTaskBuilder:
     """Builds a DB representation of the tasks in a trace"""
 
-    def __init__(self, con: sqlite3.Connection, bufsize: int = 1000) -> None:
+    def __init__(self, con: sqlite3.Connection, source_location_id: Dict[SourceLocation, int], string_id: Dict[str, int], bufsize: int = 1000) -> None:
         self.con = con
         self.bufsize = bufsize
+        self._source_location_id = source_location_id
+        self._string_id = string_id
         self._task_meta: List[Tuple[int, int, int]] = []
         self._task_links: List[Tuple[int, int]] = []
-        self._task_actions: List[Tuple[int, int, str]] = []
+        self._task_actions: List[Tuple[int, int, str, int]] = []
 
-    def add_task_metadata(self, task: int, label: int, flavour: int = -1) -> None:
-        self._task_meta.append((task, flavour, label))
+    def add_task_metadata(self, task: int, parent: Optional[int], label: str, flavour: int = -1) -> None:
+        self._task_meta.append((task, flavour, self._string_id[label]))
+        if parent is not None:
+            self._task_links.append((parent, task))
         if self._size >= self.bufsize:
             self._flush()
 
-    def add_parent_child(self, parent: int, child: int) -> None:
-        self._task_links.append((parent, child))
-        if self._size >= self.bufsize:
-            self._flush()
-
-    def add_task_action(self, task: int, action: int, time: str) -> None:
-        self._task_actions.append((task, action, time))
+    def add_task_action(self, task: int, action: TaskAction, time: str, location: SourceLocation) -> None:
+        self._task_actions.append((task, action, time, self._source_location_id[location]))
         if self._size >= self.bufsize:
             self._flush()
 
