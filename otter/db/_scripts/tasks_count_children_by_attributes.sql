881c65347d659db41ae99f1a54e39afb086435ee
Adam <73994535+adamtuft@users.noreply.github.com>
2023-10-25 16:49:51 +0100

    Return task location data in correct order

--- otter/db/_scripts/tasks_count_children_by_attributes.sql
+++ otter/db/_scripts/tasks_count_children_by_attributes.sql
@@ -1,25 +1,25 @@
 select attr.task_label
     ,attr.flavour
     ,attr.init_file
-    ,attr.init_line
     ,attr.init_func
+    ,attr.init_line
     ,attr.start_file
-    ,attr.start_line
     ,attr.start_func
+    ,attr.start_line
     ,attr.end_file
-    ,attr.end_line
     ,attr.end_func
+    ,attr.end_line
     ,child.task_label as child_task_label
     ,child.flavour as child_flavour
     ,child.init_file as child_init_file
-    ,child.init_line as child_init_line
     ,child.init_func as child_init_func
+    ,child.init_line as child_init_line
     ,child.start_file as child_start_file
-    ,child.start_line as child_start_line
     ,child.start_func as child_start_func
+    ,child.start_line as child_start_line
     ,child.end_file as child_end_file
-    ,child.end_line as child_end_line
     ,child.end_func as child_end_func
+    ,child.end_line as child_end_line
     ,count(distinct child.id) as child_tasks
 from task_attributes as attr
 left join task_attributes as child
