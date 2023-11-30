e8bf69bc657da1d41a31f11ac2b0ddb6d1621723
Adam <73994535+adamtuft@users.noreply.github.com>
2023-11-30 17:04:09 +0000

    Update event logic to include task-create. Now have separate chunk update handlers for task-enter and task-leave events. On task-enter, we check that the entered task doesn't have a chunk and then create one. On task-leave we just append to the task's chunk and return the task id

--- otter/core/event_model/task_graph_event_model.py
+++ otter/core/event_model/task_graph_event_model.py
@@ -1,25 +1,73 @@
 from __future__ import annotations
 
 from typing import Optional, Set, Tuple
-from warnings import warn
 
 from otter.core.chunk_builder import ChunkBuilderProtocol
 from otter.core.events import Event, Location
 from otter.core.tasks import Task
-from otter.definitions import (
-    Endpoint,
-    EventModel,
-    EventType,
-    NullTaskID,
-    RegionType,
-    SourceLocation,
-)
+from otter.definitions import Attr, EventModel, EventType, NullTaskID, SourceLocation
 from otter.log import logger_getter
-from otter import log
 
-from .event_model import BaseEventModel, EventModelFactory, TraceEventIterable, TaskBuilderProtocol
+from .event_model import (
+    BaseEventModel,
+    EventModelFactory,
+    TaskBuilderProtocol,
+    TraceEventIterable,
+)
 
 get_module_logger = logger_getter("task_graph_event_model")
+
+
+"""
+EVENT MODEL INCLUDING TASK-CREATE
+
+Possible events that can appear in a Task-Graph trace:
+    - task-create
+    - task-switch (enter/leave) i.e. task-begin/task-end
+    - task-sync (enter/leave sync region, or discrete i.e. just a sync without suspending) i.e. suspend/resume at a synchronisation barrier
+
+Where will these events appear in a chunk?
+
+    - task-create: in the encountering (i.e. parent) task's chunk only.
+    - task-switch: (enter/leave) in the (entered/left) task's chunk
+    - task-sync: (enter/leave/discrete) in the chunk of the task which is suspended/resumed i.e. the encountering task
+
+So a leaf task's chunk may only look like this:
+
+        task-enter
+        task-leave
+
+A branch task's chunk could look like this:
+
+        task-enter
+        task-create
+        task-sync-enter
+        task-sync-leave
+        task-create
+        task-create
+        task-create
+        task-sync-enter
+        task-sync-leave
+        task-create
+        ...
+        task-leave
+
+NOTE: there is no task-create event for the root task.
+
+So here is the logic to apply to each event:
+
+task-enter:
+    create a chunk with this event
+    *updates task start ts*
+task-sync-enter/task-sync-leave/task-sync-discrete/task-create:
+    append to encountering task's chunk (i.e. the current default logic)
+    *these events (except task-create) update task timestamps*
+    *task-sync-enter/task-sync-discrete timestamps are used for synchronising tasks*
+    *task-create timestamp is used for matching tasks to barriers*
+task-leave:
+    append to the chunk for the task which was left (recorded as event.encountering_task_id) i.e. the current default logic
+    *this event completes a chunk*
+"""
 
 
 @EventModelFactory.register(EventModel.TASKGRAPH)
@@ -34,105 +82,77 @@ class TaskGraphEventModel(BaseEventModel):
         self._return_addresses = gather_return_addresses
 
     def event_completes_chunk(self, event: Event) -> bool:
-        return (
-            event.event_type == EventType.task_switch
-            and event.endpoint == Endpoint.leave
-        )
+        return event.event_type == EventType.task_leave
 
     def event_updates_chunk(self, event: Event) -> bool:
-        return (
-            event.event_type == EventType.task_switch
-            and event.endpoint == Endpoint.enter
-        )
+        return event.event_type == EventType.task_enter
 
     def event_skips_chunk_update(self, event: Event) -> bool:
-        return (
-            event.region_type == RegionType.taskwait
-            and event.endpoint == Endpoint.enter
-        )
+        """The task-create event for the root task shouldn't be added to any chunk as it doesn't have a parent"""
+        return event.event_type == EventType.task_create and event.unique_id == 0
 
     def is_task_register_event(self, event: Event) -> bool:
-        return (
-            event.event_type == EventType.task_switch
-            and event.endpoint == Endpoint.enter
-        )
+        return event.event_type == EventType.task_create
 
     def is_task_create_event(self, event: Event) -> bool:
-        return (
-            event.event_type == EventType.task_switch
-            and event.endpoint == Endpoint.enter
-        )
+        return event.event_type == EventType.task_create
 
     def is_update_task_start_ts_event(self, event: Event) -> bool:
-        return (
-            event.event_type == EventType.task_switch
-            and event.endpoint == Endpoint.enter
-        )
+        return event.event_type == EventType.task_enter
 
     def is_update_duration_event(self, event: Event) -> bool:
         return event.event_type == EventType.task_switch
+
+    def is_chunk_start_event(self, event: Event) -> bool:
+        return event.event_type == EventType.task_enter
 
     def get_tasks_switched(self, event: Event) -> Tuple[int, int]:
         return event.parent_task_id, event.unique_id
 
     def is_task_complete_event(self, event: Event) -> bool:
-        return (
-            event.event_type == EventType.task_switch
-            and event.endpoint == Endpoint.leave
-        )
+        return event.event_type == EventType.task_leave
 
     def is_task_sync_event(self, event: Event) -> bool:
-        return event.region_type == RegionType.taskwait
-    
+        """Use task-sync-begin as the sync event as this easily captures task-create events in the parent"""
+        return event.event_type == EventType.sync_begin
+
     def is_task_suspend_event(self, event: Event) -> bool:
-        return (
-            event.region_type == RegionType.taskwait
-            and event.endpoint == Endpoint.enter
-        )
-    
+        """Matches both taskwait enter & taskwait discrete"""
+        return event.event_type == EventType.sync_begin
+
     def is_task_resume_event(self, event: Event) -> bool:
-        return (
-            event.region_type == RegionType.taskwait
-            and event.endpoint == Endpoint.leave
-        )
+        return event.event_type == EventType.sync_end
 
     def get_task_completed(self, event: Event) -> int:
-        return event.unique_id
+        return event.encountering_task_id
 
     @staticmethod
     def get_task_entered(event: Event) -> int:
-        return event.unique_id
+        return event.encountering_task_id
 
-    def get_task_data(self, event: Event) -> Task:
+    def get_task_registered_data(self, event: Event) -> Task:
         assert self.is_task_register_event(event)
         return Task(
             event.unique_id,
-            event.parent_task_id,
-            event.task_flavour,
+            event.encountering_task_id,
+            42,
             event.task_label,
             event.time,
-            SourceLocation(
-                event.task_init_file, event.task_init_func, event.task_init_line
-            ),
+            SourceLocation(event.source_file, event.source_func, event.source_line),
         )
 
-    def get_task_start_location(self, event: Event) -> SourceLocation:
-        return SourceLocation(event.source_file, event.source_func, event.source_line)
-
-    def get_task_end_location(self, event: Event) -> SourceLocation:
+    def get_source_location(self, event: Event) -> SourceLocation:
         return SourceLocation(event.source_file, event.source_func, event.source_line)
 
     def _pre_yield_event_callback(self, event: Event) -> None:
         """Called once for each event before it is sent to super().yield_chunks"""
-        if (
-            event.event_type == EventType.task_switch
-            and event.unique_id == event.parent_task_id
-        ):
-            warn(f"Task is own parent {event=}", category=Warning)
+        return
 
     def _post_yield_event_callback(self, event: Event) -> None:
         """Called once for each event after it has been sent to super().yield_chunks"""
-        if self._return_addresses is not None:
+        if self._return_addresses is not None and hasattr(
+            event, Attr.caller_return_address.value
+        ):
             address = event.caller_return_address
             if address not in self._return_addresses:
                 self._return_addresses.add(address)
@@ -144,53 +164,51 @@ class TaskGraphEventModel(BaseEventModel):
             return False
         return True
 
-    def _filter_with_callbacks(self, events_iter: TraceEventIterable) -> TraceEventIterable:
+    def _filter_with_callbacks(
+        self, events_iter: TraceEventIterable
+    ) -> TraceEventIterable:
         for location, location_count, event in events_iter:
             if self._filter_event(event):
                 self._pre_yield_event_callback(event)
                 yield location, location_count, event
                 self._post_yield_event_callback(event)
 
-    def generate_chunks(self, events_iter: TraceEventIterable, chunk_builder: ChunkBuilderProtocol, task_builder: TaskBuilderProtocol):
-        return super().generate_chunks(self._filter_with_callbacks(events_iter), chunk_builder, task_builder)
-        
+    def generate_chunks(
+        self,
+        events_iter: TraceEventIterable,
+        chunk_builder: ChunkBuilderProtocol,
+        task_builder: TaskBuilderProtocol,
+    ):
+        return super().generate_chunks(
+            self._filter_with_callbacks(events_iter), chunk_builder, task_builder
+        )
 
-@TaskGraphEventModel.update_chunks_on(event_type=EventType.task_switch)
-def update_chunks_task_switch(
+
+@TaskGraphEventModel.update_chunks_on(event_type=EventType.task_enter)
+def update_chunks_task_enter(
     event: Event,
     location: Location,
     location_count: int,
     chunk_builder: ChunkBuilderProtocol,
 ) -> Optional[int]:
-    logger = get_module_logger()
-    logger.debug(
-        "%s event_type=%s region_type=%s endpoint=%s",
-        event,
-        event.event_type,
-        event.region_type,
-        event.endpoint,
-    )
-    enclosing_key = event.parent_task_id
-    key = event.unique_id
-    if event.endpoint == Endpoint.enter:
-        if enclosing_key != NullTaskID:
-            chunk_builder.append_to_chunk(
-                enclosing_key, event, location.ref, location_count
-            )
-        try:
-            assert not chunk_builder.contains(key)
-        except AssertionError as e:
-            log.error("failed to create chunk for key %s", key)
-            log.error("time=%d key=%s already defined in chunk builder", event.time, key)
-            log.error("location: %s ref=%d", location, location.ref)
-            log.error("location_count: %s", location_count)
-            log.error("event: %s", event)
-            raise e
-        chunk_builder.new_chunk(key, event, location.ref, location_count)
-        result = None
-    elif event.endpoint == Endpoint.leave:
-        chunk_builder.append_to_chunk(key, event, location.ref, location_count)
-        result = key
-    else:
-        raise ValueError(f"unexpected endpoint: {event.endpoint}")
-    return result
+    assert hasattr(event, Attr.encountering_task_id.value)
+    task: int = event.encountering_task_id
+    assert task != NullTaskID
+    assert not chunk_builder.contains(task)
+    chunk_builder.new_chunk(task, event, location.ref, location_count)
+    return None
+
+
+@TaskGraphEventModel.update_chunks_on(event_type=EventType.task_leave)
+def update_chunks_task_leave(
+    event: Event,
+    location: Location,
+    location_count: int,
+    chunk_builder: ChunkBuilderProtocol,
+) -> Optional[int]:
+    assert hasattr(event, Attr.encountering_task_id.value)
+    task: int = event.encountering_task_id
+    assert task != NullTaskID
+    assert chunk_builder.contains(task)
+    chunk_builder.append_to_chunk(task, event, location.ref, location_count)
+    return task
