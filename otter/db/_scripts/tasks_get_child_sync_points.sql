9c1dfd2b77bf7643e988afd9c558a989c4603c7d
Adam <73994535+adamtuft@users.noreply.github.com>
2024-03-15 14:39:38 +0000

    Add duration prior to each sync point to give crt time of task as an offset into that part of a task.

--- otter/db/_scripts/tasks_get_child_sync_points.sql
+++ otter/db/_scripts/tasks_get_child_sync_points.sql
@@ -3,7 +3,11 @@ select rel.child_id
     ,sync.context_id
     ,ctx.sync_descendants
     ,ctx.sync_ts
-    ,chunk.sequence
+	,chunk.sequence
+    ,cast(crt.time as int) - cast(events_dt.start_ts as int) as child_crt_dt
+	,events_dt.start_ts
+	,events_dt.end_ts
+    ,cast(events_dt.end_ts as int) - cast(events_dt.start_ts as int) as chunk_duration
 from task
 
 -- get the child tasks of task.id
@@ -25,9 +29,44 @@ left join context as ctx
 left join chunk
     on task.id = chunk.encountering_task_id
     and sync.context_id = chunk.context_id
+
+left join (
+
+	with events as (
+		select *
+			,ROW_NUMBER() over (order by id, time) as row_number
+		from task_history
+		where action in (2, 3, 4, 5)
+			and id in (?)
+		order by id, time
+	)
+
+	select events_left.id
+		,events_left.action as parent_action_start
+		,events_right.action as parent_action_end
+		,events_left.time as start_ts
+		,events_right.time as end_ts
+
+    from events as events_left
+
+	inner join events as events_right
+		on events_left.id = events_right.id
+		and events_left.row_number = events_right.row_number-1
+
+) as events_dt
+-- V1: match sync time exactly to the time the parent was suspended
+-- 	on task.id = events_dt.id
+-- 	and ctx.sync_ts = events_dt.end_ts
+
+-- V2: match child crt_ts to between the start & end of a part of the task (to also catch unsynchronised tasks)
+	on task.id = events_dt.id
+	and crt.time >= events_dt.start_ts
+	and crt.time <= events_dt.end_ts
+	
 where task.id in (
-    1
+    ?
 )
+
 order by 1
     -- when sequence is null, these are un-synchronised tasks, so sort them last
     -- (since in our event model, a synchronisation captures all tasks so far, 
