5a79cc41e731407eeaa1557fecbf37eb40bb7bbf
Adam <73994535+adamtuft@users.noreply.github.com>
2025-06-19 12:05:48 +0100

    Simulated schedules no longer include source location table, just simulated task history

--- otter/db/_scripts/get_simulated_scheduling_states.sql
+++ otter/db/_scripts/get_simulated_scheduling_states.sql
@@ -7,14 +7,10 @@ with events as (
         and hist.id in ({placeholder})
 )
 select events_left.id
+    ,events_left.source_location_id as source_location_id_start
+    ,events_right.source_location_id as source_location_id_end
 	,events_left.action as action_start
 	,events_right.action as action_end
-	,src_left.file_name as file_name_start
-	,src_left.func_name as func_name_start
-	,src_left.line as line_start
-	,src_right.file_name as file_name_end
-	,src_right.func_name as func_name_end
-	,src_right.line as line_end
 	,events_left.time as start_ts
 	,events_right.time as end_ts
     ,events_left.cpu as cpu_start
@@ -26,10 +22,6 @@ from events as events_left
 inner join events as events_right
 	on events_left.id = events_right.id
 	and events_left.row_number = events_right.row_number-1
-left join source_location as src_left
-    on events_left.source_location_id = src_left.src_loc_id
-left join source_location as src_right
-    on events_right.source_location_id = src_right.src_loc_id
 order by events_left.id
 	,events_left.time
 ;
